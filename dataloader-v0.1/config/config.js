e16e6f9a50abddc63abd1b70dd478092a5f91222
Vishal Jariwala <vdjariwala@gmail.com>
2017-03-29 11:05:05 -0500

    removed dataloader-v0.1 and jcp-v0.1

--- dataloader-v0.1/config/config.js
+++
@@ -1,9 +0,0 @@
-var toolConfig = {
-    "webUrl" : "http://192.168.0.48:8085",
-    "elasticUrl" : "http://192.168.0.35:9200",
-    "delay": 100
-}
-
-module.exports = {
-    toolConfig : toolConfig
-}
