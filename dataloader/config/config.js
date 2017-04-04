b11f1e0e22c7c1675f81f5b19afb712aed930c5d
nikhilbhatia <nikhil.bhatia@riversand.com>
2017-04-05 01:05:31 +0200

    Changes to BSDF model and users

--- dataloader/config/config.js
+++ dataloader/config/config.js
@@ -1,9 +1,9 @@
 var toolConfig = {
-    "webUrl" : "http://192.168.0.130:8085",
-    "elasticUrl" : "http://192.168.0.18:9200",
+    "webUrl": "http://35.157.12.187:8085",
+    "elasticUrl": "http://35.156.128.250:9200",
     "delay": 100
 }
 
 module.exports = {
-    toolConfig : toolConfig
+    toolConfig: toolConfig
 }
