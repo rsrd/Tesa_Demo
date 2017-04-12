bb363ef888666f55e304cc47d26709b5e03f074a
Vishal Jariwala <vdjariwala@gmail.com>
2017-04-12 03:13:07 -0500

    updated tenant config given by RM as of 12 APR 3 AM CST

--- dataloader/config/config.js
+++ dataloader/config/config.js
@@ -1,9 +1,12 @@
 var toolConfig = {
-    "webUrl": "http://192.168.0.130:8085",
-    "elasticUrl": "http://192.168.0.18:9200",
+    "webUrl": "http://192.168.0.85:8085",
+    "elasticUrl": "http://192.168.0.85:9200",
     "delay": 100
 }
 
+//we have two options to select. 1. all 2. tenant-config
+// 1. option = all - to load model, data and configs
+// 2. option = tenant-config - to load tenant-config only
 var quiteLoadConfig = {
     "enabled": false,
     "folderName": "jcp-v2",
