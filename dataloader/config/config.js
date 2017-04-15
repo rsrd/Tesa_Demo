fbf7305a4d4a6f62dee8fca8ab55261150531c07
Vishal Jariwala <vdjariwala@gmail.com>
2017-04-15 13:03:42 -0500

    synced up ui configs to jcp-v2

--- dataloader/config/config.js
+++ dataloader/config/config.js
@@ -1,6 +1,6 @@
 var toolConfig = {
-    "webUrl": "http://172.30.6.56:8085",
-    "elasticUrl": "http://172.30.6.57:9200",
+    "webUrl": "http://192.168.0.85:8085",
+    "elasticUrl": "http://192.168.0.85:9200",
     "delay": 100
 }
 
@@ -9,12 +9,20 @@ var toolConfig = {
 // 2. option = tenant-config - to load tenant-config only
 
 var quiteLoadConfig = {
-    "enabled": false,
+    "enabled": true,
     "folderName": "jcp-v2",
     "tenantId": "jcp",
-    "option": "all",
+    "option": "tenant-config",
     "flush": "N"
-}
+ }
+
+// quiteLoadConfig = {
+//     "enabled": true,
+//     "folderName": "jcp-v2",
+//     "tenantId": "jcp",
+//     "option": "all",
+//     "flush": "N"
+// }
 
 module.exports = {
     toolConfig: toolConfig,
