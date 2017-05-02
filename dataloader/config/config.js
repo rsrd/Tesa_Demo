55388155574c5626c1601f5b59e178d3e79d9c1a
nikhilbhatia <bhatia.nikhil@gmail.com>
2017-05-02 01:46:26 -0500

    changes to v3 model reference data

--- dataloader/config/config.js
+++ dataloader/config/config.js
@@ -1,8 +1,8 @@
 var toolConfig = {
     "webUrl": "http://qa-us-east-1.riversand-dataplatform.com:8085",
     "elasticUrl": "http://192.168.0.85:9200",
-    "delay": 100,
-    "delayBetweenModelObjects": 10000
+    "delay": 10,
+    "delayBetweenModelObjects": 1000
 }
 
 //we have two options to select. 1. all 2. tenant-config
@@ -11,9 +11,9 @@ var toolConfig = {
 
 var quiteLoadConfig = {
     "enabled": true,
-    "folderName": "jcp-v2-ref",
-    "tenantId": "jcp",
-    "option": "entitymodels",
+    "folderName": "jcp-v3",
+    "tenantId": "pmqa",
+    "option": "referencedata",
 }
 
 // quiteLoadConfig = {
